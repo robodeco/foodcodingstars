834bb528391eea2092e23fd1900154d9edf6f133
Alex H <08ahayden@gmail.com>
2018-03-20 19:18:50 -0400

    Alex H Commits

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,10 +1,20 @@
 var express = require("express");
 var router = express.Router();
+var path = require("path");
 
 
-router.get("/", function(req, res){
 
-  res.render("index");
+router.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/signin.html"));
+});
+
+router.get("/user", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/userpage.html"));
+});
+
+router.get("/search", function(req, res){
+  res.sendFile(path.join(__dirname, "../public/search.html"));
+
 });
 
 module.exports = router;
