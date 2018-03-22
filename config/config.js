f131736e4a1f4f46fa100d3de4c4392e498115ab
Alex Hayden <30320883+ScruffyLookingNerfHerder@users.noreply.github.com>
2018-03-21 20:14:26 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,10 +1,6 @@
 require("dotenv").config();
 module.exports = {
   "development": {
-    // "username":"root",
-    // "password":"Irfu2008!",
-    // "database":"feedbag_db",
-    // "host" : "127.0.0.1",
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
