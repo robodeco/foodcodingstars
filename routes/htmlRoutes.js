599921ad071dee22f1e5e5f15b59ea16b7ec60de
robodeco <christopherkatalinas@tests-MacBook-Pro.local>
2018-03-22 21:15:50 -0400

    second responsive push

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,15 +4,8 @@ var path = require("path");
 module.exports = function(app) {
 
   app.get("/user", function(req, res) {
-  res.sendFile(path.join(__dirname, "../public/user.html"));
+    res.sendFile(path.join(__dirname, "../public/user.html"));
   });
-
-
-  // app.get("/", function(req, res) {
-  //   console.log("It worked!");
-  // res.sendFile(path.join(__dirname, "../public/user.html"));
-  // });
-
 
   app.get("/groceries", function(req, res) {
     console.log("You're in groceries now");
